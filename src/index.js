071e8c9e0f2721e0aa8871bf0cea0d32090934e4
ProUnebit <prounebit@yandex.ru>
2023-03-30 01:21:01 +0300

    app.use(express.static(path.join(__dirname, '/')));

--- src/index.js
+++ src/index.js
@@ -18,16 +18,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, '/')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
 // fs.readFile('./index.html');
 
-app.get("/", (req, res) => {
-    // res.send("Express on Vercel");
-    // res.sendFile('./index.html');
-    res.sendFile(path.resolve(__dirname, './index.html'));
-})
+// app.get("/", (req, res) => {
+//     // res.send("Express on Vercel");
+//     // res.sendFile('./index.html');
+//     res.sendFile(path.resolve(__dirname, './index.html'));  
+// })
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
