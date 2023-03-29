0a3bf0ebe3ee8287899e7b727391aaac8c5b2c35
ProUnebit <prounebit@yandex.ru>
2023-03-29 18:22:20 +0300

    backend complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-import './index.html';
-import './index.scss';
+// import './index.html';
+// import './index.scss';
 
-import { mult, sum } from './modules/calc';
+// import { mult, sum } from './modules/calc.js';
 
-console.log(sum(3, 4));
-console.log(mult(3, 4));
+// console.log(sum(3, 4));
+// console.log(mult(3, 4));
+
+//////////////////////////////////////
+
+
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
