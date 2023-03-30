a5bdc588070427b4b44e4309d135226e5402b8de
ProUnebit <prounebit@yandex.ru>
2023-03-30 21:22:43 +0300

    res.sendFile(path.join(__dirname + 'index.html'));

--- src/index.js
+++ src/index.js
@@ -24,10 +24,11 @@ app.use('/openai', require('./routes/openaiRoutes'));
 
 // fs.readFile('./index.html');
 
-// app.get("/", (req, res) => {
-//     // res.send("Express on Vercel");
-//     // res.sendFile('./index.html');
-//     res.sendFile(path.resolve(__dirname, './index.html'));  
-// })
+app.get("/", (req, res) => {
+    // res.send("Express on Vercel");
+    // res.sendFile('./index.html');
+    // res.sendFile(path.resolve(__dirname, './index.html')); 
+    res.sendFile(path.join(__dirname + 'index.html'));
+})
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,7 @@ function onSubmit(e) {
 }
 
 async function generateImageRequest(prompt, size) {
+
     try {
         showSpinner();
 
